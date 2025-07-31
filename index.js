df8dab049f0156b29141b8ea4786e0f735bc82c7
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-07-31 01:04:23 -0700

    Refactor HTML structure in 404, About, and Contact pages; enhance server response handling in index.js

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-console.log('hi')
-
 import http from 'http';
 import url from 'url'
-import fs from 'fs'
+import fs from 'fs/promises'
 
-// server.on('error', () => {
-    
-// })
-
-// server.on('request')
-
-// server.on('exit')
-
-// http.get('http://localhost:8080/', (response) => {
-//     fs.readFile('./index.html', 'utf-8', (error, html) => {
-//         if (error) {return}
-//         response.headers()
-//     })
-// })
-// const server = http.createServer()
-// server.listen(8080)
 const PORT = 3000
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
     response.writeHead(200, {"content-type": 'text/html'})
     let parsedURL = url.parse(request.url).pathname
 
     if (parsedURL === '/') {
-        response.end(`home`)
+        console.log('home')
+        try {
+            const data = await read('./content/index.html')
+            response.end(data)
+        }
+        catch (error) {response.end(`${error}`)}
+        
         return
     }
     else if (parsedURL === '/Contact-Me') {
-        response.end(`contact-me`)
+        console.log('contact')
+        try {
+            const data = await read('./content/contact-me.html')
+            return response.end(data)
+        }
+        catch (error) {response.end(`${error}`)}
         return
     }
     else if (parsedURL === '/About') {
-        response.end(`About`)
+        console.log('about')
+        try {
+            const data = await read('./content/about.html')
+            return response.end(data)
+        }
+        catch (error) {response.end(`${error}`)}
         return
     }
     else {
+        console.log('error')
         response.end(`error`)
     }
 })
+
+async function read(fileName) {
+    try {return await fs.readFile(`${fileName}`, 'utf-8')}
+    catch (error) {return `${error}`}
+}
 
 server.listen(PORT, 'localhost', () => {
   console.log(`Server running at http://localhost:${PORT}/`);
